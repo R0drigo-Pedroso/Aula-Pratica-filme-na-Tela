196dc99929fc06183f4f248ea9dc5ba74a0911eb
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-07 09:25:03 -0300

    Update App
    Marcação de tela,
    alinhando blocos.

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.viewLogo}>
+        <Text>Filme na Tela</Text>
+      </View>
 
-const styles = StyleSheet.create({
+      <View style={estilos.viewBotoes}>
+        <Button title="Buscar filmes" />
+        <Button title="Filmes Favoritos" />
+      </View>
+
+      <View style={estilos.viewRodape}>
+        <Button title="Privacidade" />
+        <Button title="Sobre" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "blue",
+  },
+  viewLogo: {
+    flex: 3,
+    width: "80%",
+    textAlign: "center",
+    backgroundColor: "green",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  viewBotoes: {
+    flex: 2,
+    alignItems: "flex-start",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "orange",
+    width: "80%",
+  },
+  viewRodape: {
+    flex: 0.4,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "red",
+    width: "80%",
   },
 });
