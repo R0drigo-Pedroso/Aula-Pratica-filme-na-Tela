27439c2ce6224ca2ac1a5cd85c7796af9d2605d4
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-07 10:41:31 -0300

    Configuração da Fonte
    Estilização da fonte

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+import { useFonts } from "expo-font";
 import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 const App = () => {
+  /* Inserindo fontes e carrega-lá */
+  const [fonteCarregada] = useFonts({
+    monoton: require("./assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  /* A condicional abaixo serve apenas para dar um pequeno tempo suficiente para o carregamento do arquivo de fonte antes do return componente */
+  if (!fonteCarregada) {
+    return <Text>Fonte fonte Carregada!!!</Text>;
+  }
+  /* Final inserir fontes */
+
   return (
     <SafeAreaView style={estilos.container}>
       <View style={estilos.viewLogo}>
-        <Text>Filme na Tela</Text>
+        <Text style={estilos.tituloApp}>Filme na Tela</Text>
       </View>
 
       <View style={estilos.viewBotoes}>
@@ -27,22 +39,26 @@ const estilos = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "blue",
   },
   viewLogo: {
     flex: 3,
     width: "80%",
     textAlign: "center",
-    backgroundColor: "green",
     justifyContent: "flex-end",
     alignItems: "center",
   },
+  tituloApp: {
+    fontFamily: "monoton",
+    textTransform: "uppercase",
+    fontSize: 36,
+    color: "#5451a6",
+  },
+
   viewBotoes: {
     flex: 2,
     alignItems: "flex-start",
     flexDirection: "row",
     justifyContent: "space-evenly",
-    backgroundColor: "orange",
     width: "80%",
   },
   viewRodape: {
@@ -50,7 +66,6 @@ const estilos = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    backgroundColor: "red",
     width: "80%",
   },
 });
