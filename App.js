9c77ce4963b9430cd5041bd25b34e9e7aafd67ee
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-08 10:00:13 -0300

    paginação concuida

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from "react-native";
 import FormBusca from "./src/screens/Busca/FormBusca";
 import Favoritos from "./src/screens/Favoritos/Favoritos";
 import Home from "./src/screens/Home/Home";
+import Privacidade from "./src/screens/Privacidade/Privacidade";
 import Sobre from "./src/screens/Sobre/Sobre";
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="default" />
-      <Sobre />
+      <Privacidade />
     </>
   );
 };
---
+++ src/screens/Privacidade/estilosPrivacidade.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
+
+  titulo: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginVertical: 8,
+  },
+
+  texto: {
+    marginVertical: 8,
+  },
+  nomeApp: {
+    color: "#5651a6",
+    fontWeight: "bold",
+  },
+
+  subtitulo: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginVertical: 8,
+  },
+});
+
+export default estilos;
