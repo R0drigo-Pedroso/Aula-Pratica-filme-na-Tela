358fcf610de58cecf2b60bf82f666d523c79e042
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-08 11:06:09 -0300

    instalação das biblioteca

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from "react-native";
-import FormBusca from "./src/screens/Busca/FormBusca";
-import Favoritos from "./src/screens/Favoritos/Favoritos";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Home from "./src/screens/Home/Home";
-import Privacidade from "./src/screens/Privacidade/Privacidade";
 import Sobre from "./src/screens/Sobre/Sobre";
+import Privacidade from "./src/screens/Privacidade/Privacidade";
+import Favoritos from "./src/screens/Favoritos/Favoritos";
+import FormBusca from "./src/screens/Busca/FormBusca";
 
 const App = () => {
+  /* Inicializando atráves de uma constante o gerenciador de navegação Stack (Pilha de telas) */
+  const stack = createNativeStackNavigator();
+
   return (
     <>
       <StatusBar barStyle="default" />
-      <Home />
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen name="Home" component={Home} />
+          <stack.Screen name="Sobre" component={Sobre} />
+          <stack.Screen name="Privacidade" component={Privacidade} />
+          <stack.Screen name="Favoritos" component={Favoritos} />
+          <stack.Screen name="Busca" component={FormBusca} />
+        </stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
