6b456aa90636666086321c2c5cead12b600147bd
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-11-09 10:36:52 -0300

    Exercicio
    Page busca filme configurada.
    Botão funcionando ao click

--- src/screens/Busca/FormBusca.js
+++ src/screens/Busca/FormBusca.js
@@ -1,9 +1,48 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Alert,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import estilos from "./estiloBusca";
 
 const FormBusca = () => {
+  const [text, onChangeText] = React.useState(null);
+
+  // const [procurar, setProcurar] = useState(false);
+
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={estilos.container}>
+      <Text style={estilos.titulo}>Encontre seu filme aqui!</Text>
+      <Text style={estilos.subtitulos}>
+        Localize um filme que você viu ou gostaria de ver!
+      </Text>
+
+      <View style={estilos.busca}>
+        <Ionicons
+          style={estilos.icons}
+          name="film-outline"
+          size={50}
+          color="black"
+        />
+        <TextInput
+          style={estilos.input}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Filme"
+        />
+      </View>
+
+      <Button
+        title="Procurar"
+        color="#5154a6"
+        onPress={() => Alert.alert(text)}
+      />
     </SafeAreaView>
   );
 };
--- src/screens/Busca/estiloBusca.js
+++ src/screens/Busca/estiloBusca.js
@@ -1,3 +1,40 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
+
+  titulo: {
+    fontSize: 16,
+    margin: 8,
+    textAlign: "center",
+    textTransform: "uppercase",
+  },
+
+  subtitulos: {
+    fontSize: 16,
+    textAlign: "center",
+    margin: 8,
+  },
+
+  busca: {
+    flexDirection: "row",
+  },
+
+  icons: {
+    margin: 5,
+  },
+
+  input: {
+    width: "80%",
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
+export default estilos;
